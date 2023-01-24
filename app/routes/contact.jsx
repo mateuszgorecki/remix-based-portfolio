b34057b618999bce4f9bfd5b20c5626f9c2f0917
mateusz <mateusz.gorecki.dev@gmail.com>
2023-01-24 17:09:04 +0100

    add name attrs to email inputs

--- app/routes/contact.jsx
+++ app/routes/contact.jsx
@@ -70,6 +70,7 @@ export default function ContactPage() {
               <input
                 type='email'
                 placeholder='your mail'
+                name='email'
               />
               <textarea
                 name='message'
