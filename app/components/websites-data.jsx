8971e3578d3815d48173ef8e1d98825540619a90
mateusz <mateusz.gorecki.dev@gmail.com>
2023-01-30 19:13:08 +0100

    remove backdrop filter add filter blur

--- app/components/websites-data.jsx
+++ app/components/websites-data.jsx
@@ -32,4 +32,33 @@ export const DUMMY_WEBSITES = [
     alt: 'screenshot of madbit page',
     url: 'https://www.komfis.pl',
   },
+  {
+    id: '4-website',
+    title: 'Hinter',
+    technologies: 'HTML, SCSS, JavaScript, PHP',
+    description:
+      "Website for business that offers a management tool for diagnosing, forecasting and developing an organization's potential. I was made this website while working ",
+    image: hinterPhoto,
+    alt: 'screenshot of hinter page',
+    url: 'https://www.hinter.ai',
+  },
+  {
+    id: '5-website',
+    title: 'Corelab',
+    technologies: 'HTML, SCSS, JavaScript, PHP',
+    description: 'New version of website for electronics service. Previous has been build using Divi theme and ',
+    image: corelabPhoto,
+    alt: 'screenshot of corelab page',
+    url: 'https://www.core-lab.pl',
+  },
+  {
+    id: '6-website',
+    title: 'Madbit',
+    technologies: 'HTML, SCSS, JavaScript, PHP',
+    description:
+      'Website for business that offers servicing companies in the field of cash registers, software and computer hardware.',
+    image: madbitPhoto,
+    alt: 'screenshot of madbit page',
+    url: 'https://www.komfis.pl',
+  },
 ]
